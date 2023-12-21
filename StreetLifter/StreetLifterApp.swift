268817c267831f5057e6984c03b1802d9f66a383
Artem Bariev <szyinlong@gmail.com>
2023-12-21 11:57:23 +0300

    minor fix

--- StreetLifter/StreetLifterApp.swift
+++ StreetLifter/StreetLifterApp.swift
@@ -3,13 +3,12 @@ import SwiftUI
 
 @main
 struct StreetLifterApp: App {
-    @StateObject var viewModel = TrainingSessionsManager()
-    
+   
     var body: some Scene {
         WindowGroup {
          SplashView()
                 .environment(\.colorScheme, .light)
-                .environmentObject(viewModel)
+                .environmentObject(TrainingSessionsManager())
         }
     }
 }
