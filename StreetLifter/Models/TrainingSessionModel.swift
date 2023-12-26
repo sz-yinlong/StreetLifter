8230891261a496b469a8e5e107820f556187f689
Artem Bariev <szyinlong@gmail.com>
2023-12-26 20:11:27 +0300

    updated Model

--- StreetLifter/Models/TrainingSessionModel.swift
+++ StreetLifter/Models/TrainingSessionModel.swift
@@ -1,11 +1,19 @@
 import Foundation
 
 struct TrainingSession: Hashable, Identifiable {
-    let id = UUID()
+    var id = UUID()
     let date: String
     let reps: [Int]
     let weight: [Int]
     let totalReps: Int
+    
+    init(id: UUID = UUID(), date: String, reps: [Int], weight: [Int], totalReps: Int) {
+           self.id = id
+           self.date = date
+           self.reps = reps
+           self.weight = weight
+           self.totalReps = totalReps
+       }
 }
 
 enum ExerciseType {
