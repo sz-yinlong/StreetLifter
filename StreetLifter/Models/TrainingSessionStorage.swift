f2c11f7fdbc9e61dbeca20e4617195b815665a69
Artem Bariev <szyinlong@gmail.com>
2023-12-26 20:11:42 +0300

    updated sessionStorage

--- StreetLifter/Models/TrainingSessionStorage.swift
+++ StreetLifter/Models/TrainingSessionStorage.swift
@@ -5,18 +5,28 @@ import Foundation
 final class TrainingSessionStorage: ObservableObject {
     
     func saveSession(session:[TrainingSession], forKey key: String) {
-        let sessionData: [[String: Any]] = session.map { ["date": $0.date, "reps": $0.reps, "weight": $0.weight, "totalReps": $0.totalReps] }
+        let sessionData: [[String: Any?]] = session.map {
+            [
+                "id": $0.id.uuidString,
+                "date": $0.date,
+                "reps": $0.reps,
+                "weight": $0.weight,
+                "totalReps": $0.totalReps
+            ]
+        }
         UserDefaults.standard.set(sessionData, forKey: key)
     }
     
     func retrieveSessions(forKey key: String) -> [TrainingSession] {
         if let savedSessions = UserDefaults.standard.array(forKey: key) as? [[String: Any]] {
             return savedSessions.compactMap { sessionData in
-                if let date = sessionData["date"] as? String,
+                if let idString = sessionData["id"] as? String,
+                   let id = UUID(uuidString: idString),
+                   let date = sessionData["date"] as? String,
                    let reps = sessionData["reps"] as? [Int],
                    let weight = sessionData["weight"] as? [Int],
                    let totalReps = sessionData["totalReps"] as? Int {
-                    return TrainingSession(date: date, reps: reps, weight: weight, totalReps: totalReps)
+                    return TrainingSession(id: id, date: date, reps: reps, weight: weight, totalReps: totalReps)
                 }
                 return nil
             }
