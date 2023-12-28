477f8ed64441b4c884aff6554444f2d3e47e777e
Artem Bariev <szyinlong@gmail.com>
2023-12-28 18:26:47 +0300

    training views updated

--- StreetLifter/Views/PullupsTrainingView.swift
+++ StreetLifter/Views/PullupsTrainingView.swift
@@ -4,10 +4,17 @@ import SwiftUI
 
 struct PullupsTrainingView: View {
     @EnvironmentObject var trainingSessionsManager: TrainingSessionsManager
+    @State private var showingLevels = false // Состояние для отображения модального окна
 
     var body: some View {
-        BaseTrainingView<BaseTrainingViewModel, Any>(viewModel: trainingSessionsManager.pullupsViewModel, backgroundColor: Constants.bitterSweet.opacity(1))
-            .toolbar(.hidden, for: .tabBar)
-            .navigationTitle(R.string.localizable.pullups())
+        BaseTrainingView<BaseTrainingViewModel, Any>(
+            viewModel: trainingSessionsManager.pullupsViewModel,
+            backgroundColor: Constants.bitterSweet.opacity(1)
+        )
+        .toolbar(.hidden, for: .tabBar)
+        .navigationTitle(R.string.localizable.pullups())
+        .sheet(isPresented: $showingLevels) {
+            ProgramView().environmentObject(trainingSessionsManager.pullupsViewModel)
+        }
     }
 }
