6bbe1eb8e8e6f2e9775453e2c6638ca98bf51355
Artem Bariev <szyinlong@gmail.com>
2024-01-08 16:37:07 +0300

    minor refactoring

--- StreetLifter/Base/BaseTrainingView.swift
+++ StreetLifter/Base/BaseTrainingView.swift
@@ -12,15 +12,11 @@ struct BaseTrainingView<ViewModel: BaseTrainingViewModel, TrainingViewModelProto
     var showProgramView: (() -> Void)?
     var backgroundColor: Color
        
-       init(viewModel: ViewModel, backgroundColor: Color = .secondary) {
-           let storage = TrainingSessionStorage()
-           
-           _viewModel = StateObject(wrappedValue: viewModel)
-           self.backgroundColor = backgroundColor
-       }
+    init(viewModel: ViewModel, backgroundColor: Color = .secondary) {
+        _viewModel = StateObject(wrappedValue: viewModel)
+        self.backgroundColor = backgroundColor
+    }
 
-    
-    
     var body: some View {
         NavigationStack {
             if #available(iOS 17.0, *) {
@@ -36,7 +32,7 @@ struct BaseTrainingView<ViewModel: BaseTrainingViewModel, TrainingViewModelProto
                             .padding(.bottom, 250)
                      
                         NavigationLink(destination: TabBar()) {
-                           backgroundColor
+                            backgroundColor
                                 .frame(maxWidth: 300, maxHeight: 50)
                                 .cornerRadius(10)
                                 .overlay(
@@ -53,122 +49,121 @@ struct BaseTrainingView<ViewModel: BaseTrainingViewModel, TrainingViewModelProto
                         viewModel.resetReps()
                     }
                 } else {
-                        VStack {
-                            HStack(spacing: 12) {
-                                ForEach(Array(viewModel.mutableRepetitions.enumerated()), id: \.offset) { index, repetition in
-                                    VStack {
-                                        Button(action: {
-                                            withAnimation {
-                                                viewModel.currentSetIndex = index
-                                                viewModel.reps = viewModel.mutableRepetitions[index]
-                                            }
-                                        }) {
-                                            RoundedRectangle(cornerRadius: 10)
-                                                .fill(index == viewModel.currentSetIndex ? backgroundColor : Color.secondary.opacity(0.15))
-                                                .frame(maxWidth: 60, maxHeight: 55)
-                                                .overlay(
-                                                    Text("\(repetition)")
-                                                        .font(.title)
-                                                        .foregroundColor(index == viewModel.currentSetIndex ? .white : .primary)
-                                                )
+                    VStack {
+                        HStack(spacing: 12) {
+                            ForEach(Array(viewModel.mutableRepetitions.enumerated()), id: \.offset) { index, repetition in
+                                VStack {
+                                    Button(action: {
+                                        withAnimation {
+                                            viewModel.currentSetIndex = index
+                                            viewModel.reps = viewModel.mutableRepetitions[index]
                                         }
-                                        Text(index < viewModel.currenSessionWeight.count ? "\(viewModel.currenSessionWeight[index])kg" : "")
-                                            .font(.subheadline)
-                                            .foregroundColor(.primary)
-                                            .frame(height: 20)
+                                    }) {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .fill(index == viewModel.currentSetIndex ? backgroundColor : Color.secondary.opacity(0.15))
+                                            .frame(maxWidth: 60, maxHeight: 55)
+                                            .overlay(
+                                                Text("\(repetition)")
+                                                    .font(.title)
+                                                    .foregroundColor(index == viewModel.currentSetIndex ? .white : .primary)
+                                            )
                                     }
+                                    Text(index < viewModel.currenSessionWeight.count ? "\(viewModel.currenSessionWeight[index])kg" : "")
+                                        .font(.subheadline)
+                                        .foregroundColor(.primary)
+                                        .frame(height: 20)
                                 }
                             }
-                            .padding(.vertical, 8)
-                            .padding(.horizontal, 8)
+                        }
+                        .padding(.vertical, 8)
+                        .padding(.horizontal, 8)
                             
-                            Spacer()
-                            Text(viewModel.currentSetIndex < viewModel.mutableRepetitions.count ? "\(viewModel.mutableRepetitions[viewModel.currentSetIndex])" : "0")
-                                .font(.system(size: 80))
-                                .foregroundStyle(.primary)
-                                .bold()
-                                .fontDesign(.rounded)
+                        Spacer()
+                        Text(viewModel.currentSetIndex < viewModel.mutableRepetitions.count ? "\(viewModel.mutableRepetitions[viewModel.currentSetIndex])" : "0")
+                            .font(.system(size: 80))
+                            .foregroundStyle(.primary)
+                            .bold()
+                            .fontDesign(.rounded)
                                
+                        Spacer()
+                            
+                        HStack(alignment: .center) {
+                            Button(action: {
+                                viewModel.decrementReps()
+                            }) {
+                                Image(systemName: "minus.circle")
+                                    .foregroundColor(.primary)
+                                    .font(.system(size: 50))
+                            }
+                            .padding(.leading, 10)
                             Spacer()
-                            
-                            HStack(alignment: .center) {
-                                Button(action: {
-                                    viewModel.decrementReps()
-                                }) {
-                                    Image(systemName: "minus.circle")
-                                        .foregroundColor(.primary)
-                                        .font(.system(size: 50))
-                                }
-                                .padding(.leading, 10)
-                                Spacer()
                                 
-                                Text(R.string.localizable.repsCaps())
+                            Text(R.string.localizable.repsCaps())
                                 
-                                Spacer()
+                            Spacer()
                                 
-                                Button(action: {
-                                    viewModel.incrementReps()
-                                }) {
-                                    Image(systemName: "plus.circle")
-                                        .foregroundColor(.primary)
-                                        .font(.system(size: 50))
-                                }
-                                .padding(.trailing, 10)
+                            Button(action: {
+                                viewModel.incrementReps()
+                            }) {
+                                Image(systemName: "plus.circle")
+                                    .foregroundColor(.primary)
+                                    .font(.system(size: 50))
                             }
-                            .font(.largeTitle)
-                            .padding()
+                            .padding(.trailing, 10)
+                        }
+                        .font(.largeTitle)
+                        .padding()
                             
+                        HStack {
                             HStack {
-                                HStack {
-                                    Toggle("", isOn: $viewModel.isWeightAdded)
-                                        .labelsHidden()
-                                        .toggleStyle(.switch)
-                                        .tint(Color(.systemBlue))
-                                    Text(R.string.localizable.addWeight())
-                                }
-                                Picker("Weight", selection: $viewModel.selectedWeightIndex) {
-                                    ForEach(0 ..< viewModel.availableWeights.count, id: \.self) {
-                                        index in
-                                        Text("\(viewModel.availableWeights[index])kg").tag(index)
-                                    }
-                                }
-                                .onChange(of: viewModel.selectedWeightIndex, perform: { newValue in
-                                    viewModel.tempSelectedWeight = viewModel.availableWeights[newValue]
-                                    print("Выбранный вес: \(viewModel.availableWeights[newValue])кг")
-                                })
-                                .onChange(of: viewModel.selectedLevel) { _ in
-                                    viewModel.selectLevel(level: viewModel.selectedLevel)
-                                }
-                                
-                                .disabled(!viewModel.isWeightAdded)
-                                .frame(width: 100, height: 100)
-                                .pickerStyle(WheelPickerStyle())
-                                .padding()
-                                .opacity(viewModel.isWeightAdded ? 1 : 0.2)
+                                Toggle("", isOn: $viewModel.isWeightAdded)
+                                    .labelsHidden()
+                                    .toggleStyle(.switch)
+                                    .tint(Color(.systemBlue))
+                                Text(R.string.localizable.addWeight())
                             }
+                            Picker("Weight", selection: $viewModel.selectedWeightIndex) {
+                                ForEach(0 ..< viewModel.availableWeights.count, id: \.self) {
+                                    index in
+                                    Text("\(viewModel.availableWeights[index])kg").tag(index)
+                                }
+                            }
+                            .onChange(of: viewModel.selectedWeightIndex, perform: { newValue in
+                                viewModel.tempSelectedWeight = viewModel.availableWeights[newValue]
+                                print("Выбранный вес: \(viewModel.availableWeights[newValue])кг")
+                            })
+                            .onChange(of: viewModel.selectedLevel) { _ in
+                                viewModel.selectLevel(level: viewModel.selectedLevel)
+                            }
+                                
+                            .disabled(!viewModel.isWeightAdded)
+                            .frame(width: 100, height: 100)
+                            .pickerStyle(WheelPickerStyle())
+                            .padding()
+                            .opacity(viewModel.isWeightAdded ? 1 : 0.2)
+                        }
                             
-                            VStack {
-                                Button(action: {
-                                    if viewModel.canAddSet {
-                                        viewModel.trainingCompleted = false
-                                    } else {
-                                        viewModel.trainingCompleted = true
-                                        viewModel.saveTrainingSession()
-                                    }
-                                    viewModel.saveRepsForCurrentSession()
-                                }) {
-                                    Text(R.string.localizable.done()
-                                    )
-                                    .foregroundColor(.white)
-                                    .font(.headline)
-                                    .padding()
-                                    .frame(maxWidth: 300, maxHeight: 50)
-                                    .cornerRadius(10)
+                        VStack {
+                            Button(action: {
+                                if viewModel.canAddSet {
+                                    viewModel.trainingCompleted = false
+                                } else {
+                                    viewModel.trainingCompleted = true
+                                    viewModel.saveTrainingSession()
                                 }
-                                .background(Constants.acidGreen)
+                                viewModel.saveRepsForCurrentSession()
+                            }) {
+                                Text(R.string.localizable.done()
+                                )
+                                .foregroundColor(.white)
+                                .font(.headline)
+                                .padding()
+                                .frame(maxWidth: 300, maxHeight: 50)
                                 .cornerRadius(10)
-                                
                             }
+                            .background(Constants.acidGreen)
+                            .cornerRadius(10)
+                        }
                     }
                         
                     .padding()
@@ -178,15 +173,13 @@ struct BaseTrainingView<ViewModel: BaseTrainingViewModel, TrainingViewModelProto
         .navigationBarBackButtonHidden()
         .fontDesign(.rounded)
         
-        
-            
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
                 Button {
                     dismiss()
                 } label: {
                     Image(systemName: "chevron.backward")
-                } 
+                }
                 .fontWeight(.medium)
             }
             ToolbarItem(placement: .topBarTrailing) {
@@ -201,7 +194,7 @@ struct BaseTrainingView<ViewModel: BaseTrainingViewModel, TrainingViewModelProto
                     ProgramView().environment(\.colorScheme, .dark).environmentObject(viewModel)
                 }
             }
-        } .accentColor(Constants.acidGreen)
+        }.accentColor(Constants.acidGreen)
 
         .onAppear {
             viewModel.startNewSession()
